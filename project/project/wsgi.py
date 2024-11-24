40c1f85c9c9ef9c5e7c9f77955e2790be00ba80e
railgunglm <gitlun100@gmail.com>
2024-11-24 20:08:00 +0000

    attempt #10

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -17,6 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
-
 app = application
